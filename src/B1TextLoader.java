48f52556b49dae58aedf3c1dd22414436e0f41f6
Andrew Keenan <keenanandrew@gmail.com>
2024-10-09 10:10:09 +0100

    checking in

--- src/B1TextLoader.java
+++ src/B1TextLoader.java
@@ -1,6 +1,18 @@
+import java.io.BufferedReader;
+import java.io.File
+import java.io.FileReader;
+
 public class B1TextLoader {
 
-    public static main void(String[] args) {
+    public static void main(String[] args) {
+        B1TextLoader loader = new B1TextLoader();
+        loader.loadTextFile("BasicTextFile.txt");
+    }
+    public void loadTextFile(String filename) {
+        System.out.println("Loading your file, beep boop...");
 
+        File f = new File(filename);
+        BufferedReader br = new BufferedReader(new FileReader(f));
     }
 }
+
