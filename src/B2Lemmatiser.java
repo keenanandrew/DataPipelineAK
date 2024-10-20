1cb768658d3a235a29565961a200d99d7729562c
Andrew Keenan <keenanandrew@gmail.com>
2024-10-20 18:55:19 +0100

    Now with start of lemmatisation block

--- src/B2Lemmatiser.java
+++ src/B2Lemmatiser.java
@@ -6,17 +6,25 @@ public class B2Lemmatiser {
     public static void main(String[] args) {
         B2Lemmatiser lemmatiser = new B2Lemmatiser();
         lemmatiser.startLemmatisation("DataStore.json"); // for now, just prints the filename
+        lemmatiser.lemmatiseSingleDocument("DataStore.json");
 
 
     }
 
     private void startLemmatisation(String filename) {
+
         System.out.println(filename);
         JSONIOHelper jsonIO = new JSONIOHelper();
         jsonIO.loadJSON("DataStore.json");
 
         ConcurrentHashMap<String, String> documents = jsonIO.getDocumentsFromJSONStructure();
         System.out.println("And done something with a CHM");
+
+    }
+
+    private String lemmatiseSingleDocument(String text) {
+        System.out.println("Yep works - look: " + text);
+        return text;
 
 
     }
